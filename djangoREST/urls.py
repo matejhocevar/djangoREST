eb3d5e5a0ebbc85e7542ea2b4e579ea77b506c1b
Matej Hočevar <mh3500@student.uni-lj.si>
2016-01-12 23:24:03 +0100

    url for admin panel added

--- djangoREST/urls.py
+++ djangoREST/urls.py
@@ -5,12 +5,13 @@ from djangoRESTApp import views
 
 # API endpoints
 urlpatterns = format_suffix_patterns([
-    url(r'^$', views.Home),
-    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail')
+	url(r'^$', views.Home),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+	url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
+	url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+	url(r'^users/$', views.UserList.as_view(), name='user-list'),
+	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail')
 ])
 
 # Login and logout views for the browsable API
